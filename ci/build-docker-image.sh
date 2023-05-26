fbad952b79cff715966a670f29dc31fa39969422
coranos <coranos@users.noreply.github.com>
2023-05-25 19:07:11 -0500

    merging v25

--- ci/build-docker-image.sh
+++ ci/build-docker-image.sh
@@ -8,7 +8,7 @@ fi
 
 dockerFile="$1"
 dockerTag="$2"
-githubTag="ghcr.io/${GITHUB_REPOSITORY}/${dockerTag#*/}"
+githubTag="ghcr.io/$bananocoin/${dockerTag#*/}"
 shift
 shift
 
@@ -20,7 +20,7 @@ echo "Pulling $githubTag"
 "$scripts"/custom-timeout.sh 20 docker pull "${githubTag}" || true
 
 echo "Building $githubTag"
-"$scripts"/custom-timeout.sh 60 docker build "$@" --build-arg REPOSITORY=${GITHUB_REPOSITORY} -f "${dockerFile}" -t "${githubTag}" --cache-from "${githubTag}" .
+"$scripts"/custom-timeout.sh 60 docker build "$@" --build-arg REPOSITORY=$bananocoin -f "${dockerFile}" -t "${githubTag}" --cache-from "${githubTag}" .
 
 echo "Tagging ${dockerTag} from ${githubTag}"
 docker tag $githubTag $dockerTag
