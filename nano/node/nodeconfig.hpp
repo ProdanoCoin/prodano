2934b3ebc88084327062c07ea6e6786f234e40dc
coranos <coranos@users.noreply.github.com>
2023-05-28 10:57:50 -0500

    debugging

--- nano/node/nodeconfig.hpp
+++ nano/node/nodeconfig.hpp
@@ -54,11 +54,11 @@ public:
 	std::vector<nano::account> preconfigured_representatives;
 	unsigned bootstrap_fraction_numerator{ 1 };
 	nano::amount receive_minimum{ nano::RAW_ratio };
-	nano::amount vote_minimum{ nano::GRAW_ratio };
+	nano::amount vote_minimum{ nano::MBAN_ratio };
 	nano::amount rep_crawler_weight_minimum{ "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF" };
 	std::chrono::milliseconds vote_generator_delay{ std::chrono::milliseconds (100) };
 	unsigned vote_generator_threshold{ 3 };
-	nano::amount online_weight_minimum{ 60000 * nano::GRAW_ratio };
+	nano::amount online_weight_minimum{ 60000 * nano::MBAN_ratio };
 	unsigned election_hint_weight_percent{ 50 };
 	unsigned password_fanout{ 1024 };
 	unsigned io_threads{ std::max (4u, nano::hardware_concurrency ()) };
