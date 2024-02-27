631bbb107a05913228e41e969966dd5178a04125
coranos <coranos@users.noreply.github.com>
2024-02-26 21:50:37 -0600

    merging

--- nano/lib/config.hpp
+++ nano/lib/config.hpp
@@ -381,7 +381,7 @@ public:
 	uint8_t const protocol_version = 0x14;
 	/** Minimum accepted protocol version */
 	uint8_t const protocol_version_min = 0x12;
-	
+
 	/** Minimum accepted protocol version used when bootstrapping */
 	uint8_t const bootstrap_protocol_version_min = 0x13;
 };
